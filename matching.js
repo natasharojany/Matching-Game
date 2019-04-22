4866f05ba756e88beb3a26796b6e8f3870bb0a60
Lance Davenport <lancedavenport20@k12.xrds.org>
2019-04-22 09:31:16 -0700

    added stuff that didnt work

--- matching.js
+++ matching.js
@@ -1,1 +1,16 @@
-const cardValue = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J', 'K', 'K', 'L', 'L', 'M', 'M', 'N', 'N', 'O', 'O', 'P', 'P', 'Q', 'Q', 'R', 'R', 'S', 'S', 'T', 'T', 'U', 'U', 'V', 'V', 'W', 'W', 'X', 'X', 'Y', 'Y', 'Z', 'Z', 'AA', 'AA', 'BB', 'BB', 'CC', 'CC', 'DD', 'DD', 'EE', 'EE', 'FF', 'FF', 'GG', 'GG', 'HH', 'HH', 'II', 'II', 'JJ', 'JJ']
+const cardValues = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J', 'K', 'K', 'L', 'L', 'M', 'M', 'N', 'N', 'O', 'O', 'P', 'P']
+
+var memoryValues = [];
+var memoryTileIds = [];
+var tilesFlipped = 0;
+
+function newBoard() {
+	tilesFlipped = 0;
+  cardValues = _.shuffle(cardValues);
+
+function flip(cardId){
+  value = cardValues[cardId];
+  card = document.querySelector("#card" + cardId);
+  card.innerText = value;
+ 
+}
