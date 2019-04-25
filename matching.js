db178fa2a591c4ba6031454a8a24dba39bf1ee98
Lance Davenport <lancedavenport20@k12.xrds.org>
2019-04-25 09:01:02 -0700

    Added some stuff

--- matching.js
+++ matching.js
@@ -1,31 +1,16 @@
-let cardValues = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J', 'K', 'K', 'L', 'L', 'M', 'M', 'N', 'N', 'O', 'O', 'P', 'P']
+const cardValues = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J', 'K', 'K', 'L', 'L', 'M', 'M', 'N', 'N', 'O', 'O', 'P', 'P']
 
 var memoryValues = [];
 var memoryTileIds = [];
 var tilesFlipped = 0;
 
-Array.prototype.shuffle = function() {
-    var input = this;
-     
-    for (var i = input.length-1; i >=0; i--) {
-     
-        var randomIndex = Math.floor(Math.random()*(i+1)); 
-        var itemAtIndex = input[randomIndex]; 
-         
-        input[randomIndex] = input[i]; 
-        input[i] = itemAtIndex;
-    }
-    return input;
-}
- 
-var cardValues = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J', 'K', 'K', 'L', 'L', 'M', 'M', 'N', 'N', 'O', 'O', 'P', 'P']
-cardValues.shuffle();
- 
-       
-
+function shuffle() {
+	cardValues = Collections.shuffle(cardValues)
+}       
+newBoard()
 function newBoard() {
 	tilesFlipped = 0;
-	cardValues.shuffle();
+	shuffle()
 }
 
 function flip(cardId){
@@ -33,4 +18,15 @@ function flip(cardId){
   card = document.querySelector("#card" + cardId);
   card.innerText = value;
  
+}
+
+function timer(){
+    var sec = 60;
+    var timer = setInterval(function(){
+        document.getElementById('safeTimerDisplay').innerHTML='00:'+sec;
+        sec--;
+        if (sec < 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
 }
