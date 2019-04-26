b753f57aee118c249e3e43b7db3fe3656c233120
Natasha Rojany <NatashaRojany20@k12.xrds.org>
2019-04-26 09:25:27 -0700

    delete

--- matching.js
+++ matching.js
@@ -25,14 +25,6 @@ function flip(cardId){
 }
 
 
-{
-function shuffle(array)
-let arr = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J', 'K', 'K', 'L', 'L', 'M', 'M', 'N', 'N', 'O', 'O', 'P', 'P']
-  shuffle(arr);
-  arr = ['K', 'L','C','A', 'H', 'L', 'M', 'M', 'N', 'E', 'F', 'N', 'A', 'B',  'C', 'D', 'D', 'E',  'F', 'G', 'O', 'P','B','H', 'I', 'J', 'K', 'P', 'I', 'J','G','O']
-}
-
-
 
 function timer(){
     var sec = 60;
