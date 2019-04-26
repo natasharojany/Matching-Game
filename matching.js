03bb07c12776882740dd2ab0281000a7d5cb0f19
Daniela Lubezki <Danielalubezki20@k12.xrds.org>
2019-04-26 09:58:44 -0700

    timer

--- matching.js
+++ matching.js
@@ -1,4 +1,4 @@
-var { Timer } = require('lib/easytimer/dist/easytimer.min.js');                
+                
 
 class Matching {
 	constructor() {
@@ -62,19 +62,6 @@ function shuffle(array)
 let arr = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J', 'K', 'K', 'L', 'L', 'M', 'M', 'N', 'N', 'O', 'O', 'P', 'P']
   shuffle(arr);
   arr = ['K', 'L','C','A', 'H', 'L', 'M', 'M', 'N', 'E', 'F', 'N', 'A', 'B',  'C', 'D', 'D', 'E',  'F', 'G', 'O', 'P','B','H', 'I', 'J', 'K', 'P', 'I', 'J','G','O']
-}
-
-
-
-function timer(){
-    var sec = 60;
-    var timer = setInterval(function(){
-        document.getElementById('safeTimerDisplay').innerHTML='00:'+sec;
-        sec--;
-        if (sec < 0) {
-            clearInterval(timer);
-        }
-    }, 1000);
 }
 
 
