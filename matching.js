fd4c591fd83929a765c48e167ea36e8db4bb61d1
Lance Davenport <lancedavenport20@k12.xrds.org>
2019-04-30 08:20:10 -0700

    Formatted

--- matching.js
+++ matching.js
@@ -57,16 +57,7 @@ function moveCounter(){
 
 let count = -1
 
-function timer(){
-    var sec = 60;
-    var timer = setInterval(function(){
-        document.getElementById('safeTimerDisplay').innerHTML='00:'+sec;
-        sec--;
-        if (sec < 0) {
-            clearInterval(timer);
-        }
-    }, 1000);
-}
+
 
 
 function flipBack(cardId) {
